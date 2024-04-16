f92b08d9047ac7ff24a54c3829cfd2d07f749d01
chowankumar <chowanmalhani5@gmail.com>
2024-04-16 23:50:41 +0500

    set server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+
+//app config
+const app = express();
+const port = 4000;
+
+//middlware
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+
+app.listen(port,()=>{
+    console.log(`server started on http://localhost:${port}`)
+})
+ 
