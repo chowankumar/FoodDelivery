fddd9cc444b70c83988ae31c4cf6a581746a7402
chowankumar <chowanmalhani5@gmail.com>
2024-04-19 15:55:47 +0500

    integrate api in addcart removeFromCart function on frontend

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-
 export const StoreContext = createContext(null)
-
 const StoreContextProvider = (props) => {
 
 
@@ -12,16 +10,25 @@ const StoreContextProvider = (props) => {
     const [food_list,setFoodList] = useState([]);
 
     //add to cart
-    const addToCart = (itemId) => {
+    const addToCart = async(itemId)=>{
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         } else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
+        if(token){
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
+
+
     }
     //remove from the cart
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+      if(token){
+        await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+
+      }
     }
 
     const getTotalCartAmount = () => {
