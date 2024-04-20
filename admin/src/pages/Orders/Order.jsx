e0e13b38acda5ed95454e84fbc245ebaf7ae7cbb
chowankumar <chowanmalhani5@gmail.com>
2024-04-20 14:49:11 +0500

    link api to updateStatus of the order in the admin pannel

--- admin/src/pages/Orders/Order.jsx
+++ admin/src/pages/Orders/Order.jsx
@@ -23,6 +23,19 @@ const Order = ({ url }) => {
   }
 
 
+  const statusHandler=async(event,orderId)=>{
+    const response = await axios.post(url+"/api/order/status",
+    {
+      orderId,
+      status:event.target.value
+    })
+    if(response.data.success){
+      await fetchAllOrders();
+    }
+
+  }
+
+
   useEffect(() => {
     fetchAllOrders();
   }, [])
@@ -59,7 +72,7 @@ const Order = ({ url }) => {
               </div>
               <p>Items : {order.items.length}</p>
               <p>${order.amount}</p>
-              <select name="" id="">
+              <select onChange={(event)=>statusHandler(event,order._id)}  value={order.status}>
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out For Delivery">Out For Delivery</option>
                 <option value="Delivered">Delivered</option>
