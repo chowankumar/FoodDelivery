9e39fb5e3708a2e5b5ccd1dd699cbf5101162ef8
chowankumar <chowanmalhani5@gmail.com>
2024-07-07 00:42:28 +0500

    add to cart and delete from the cart with the help od contextStore

--- src/Components/FoodItems.jsx
+++ src/Components/FoodItems.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext} from 'react'
 import { assets } from '../assets/assets'
+import { StoreContext } from '../context/StoreContext';
 
-const FoodItems = ({ id, name, price, image, desc, category }) => {
-  const[itemCount,setItemCount] = useState(0);
+const FoodItems = ({ id, name, price, image, desc,}) => {
+
+  const {cartItems,removeFromCart,addToCart} = useContext(StoreContext);
   return (
 
     <div className='rounded-[10px] bg-white shadow-md relative'>
 
       <img src={image} alt="" className='rounded-tl-[15px] rounded-tr-[15px] max-h-[300px]' />
       {
-       !itemCount? <img src={assets.add_icon_white} alt=""  onClick={()=>setItemCount(prev=>prev+1)} className='absolute bg-white top-[198px] 
+       !cartItems[id]? <img src={assets.add_icon_white} alt=""  onClick={()=>addToCart(id)} className='absolute bg-white top-[198px] 
        right-[20px] rounded-full h-10'/>
       :<div className='flex items-center gap-2 absolute top-[198px] right-[20px] bg-white px-2 py-2 rounded-full'>
-         <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-           <p className='font-[600]'>{itemCount}</p>
-           <img  onClick={()=>setItemCount(prev=>prev+1)}src={assets.add_icon_green} alt="" />
+         <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+           <p className='font-[600]'>{cartItems[id]}</p>
+           <img  onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
          </div>
       }
       <div className='p-4 flex flex-col gap-2 '>
