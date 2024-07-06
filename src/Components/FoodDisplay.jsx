af2d253ef863790503c57a9241d9b044f1f9f6a4
chowankumar <chowanmalhani5@gmail.com>
2024-07-07 01:42:48 +0500

    add flter function which filter the dishes categoy wiese

--- src/Components/FoodDisplay.jsx
+++ src/Components/FoodDisplay.jsx
@@ -3,19 +3,23 @@ import { StoreContext } from "./../context/StoreContext"
 import FoodItems from './FoodItems'
 
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext)
     return (
         <div>
             <h1 className=' text-[35px] font-[600] mb-4'>Top dishes near you</h1>
             <div className='grid grid-cols-4 gap-4'>
-                
+
                 {
                     food_list.map((item, index) => {
-                        return (
-                            <FoodItems key={index} id={item._id} name={item.name} image={item.image}
+                         
+                            if(category === "ALL" || category === item.category){
+                               return  <FoodItems key={index} id={item._id} name={item.name} image={item.image}
                                 price={item.price} desc={item.description} category={item.category} />
-                        )
+
+                            }
+                           
+                        
                     })
 
                 }
