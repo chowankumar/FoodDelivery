682cf15878d9a7884cc459ddbf9255465d89e9b8
chowankumar <chowanmalhani5@gmail.com>
2024-07-06 17:51:43 +0500

    header section done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,10 @@ const Header = () => {
   return (
     <div className='header bg-custom-header bg-no-repeat bg-contain h-[34vw] w-[80%] relative m-auto my-7.5 mx-auto'>
     <div className="header-contents  absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw]">
-        <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the satisfy your dinning experience, one delicious meal at a time.</p>
-        <button>View Menu</button>
+        <h2 className='font-[500] text-white text-[75px]'>Order your favourite food here</h2>
+        <p className='text-white text-[18px]'>Choose from a diverse menu featuring a delectable array of dishes crafted with the satisfy your dinning experience, one delicious meal at a time.</p>
+        <button className='border-none text-[#747474] font-[500] px-6 py-4 bg-white 
+        text-[17px] rounded-[50px] '>View Menu</button>
     </div>
 </div>
   )
