c3046761681cf4c7ebee4d7b5efd2f7403bde99c
chowankumar <chowanmalhani5@gmail.com>
2024-07-08 04:06:57 +0500

    make navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,12 +6,13 @@ const Navbar = () => {
     const [menu, setMenu] = useState("")
     return (
 
-        <div className='p-8 flex flex-col gap-2 lg:flex-row  justify-between items-center   m-auto '>
+        <div className='p-8 flex  gap-2  justify-between items-center m-auto'>
 
             <div className="left-nav">
                 <img src={assets.logo} alt="" />
             </div>
-            <div className="middle-nav flex  list-none gap-6 font-semibold text-[19px]">
+
+            <div className="middle-nav hidden xl:flex list-none lg:gap-4 gap-6 font-semibold text-[19px]">
 
                 <li onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""} cursor-pointer`}>home</li>
                 <li onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""} cursor-pointer`}>menu</li>
@@ -21,7 +22,10 @@ const Navbar = () => {
 
 
             </div>
-            <div className="right-nav flex items-center justify-between gap-16 lg:gap-8">
+
+
+
+            <div className="right-nav flex items-center justify-between gap-4 xl:gap-16">
                 <div className="search">
                     <img src={assets.search_icon} alt="" />
                 </div>
@@ -31,7 +35,7 @@ const Navbar = () => {
                      top-[-8px] right-[-8px]"></div>
                 </div>
                 <div className="signin-button">
-                    <button className=' border border-gray-400 px-6 py-2 rounded-full hover:bg-[#fff4f2]'>sign in</button>
+                    <button className=' border border-gray-400 px-4 py-2 xl:px-6 xl:py-2 rounded-full hover:bg-[#fff4f2]'>sign in</button>
                 </div>
             </div>
 
